7031a5e57c05f61d5283e8205d6874c03a1b2b09
James Ferry <jamesferry84@hotmail.com>
2021-10-17 16:01:36 +0100

    adding conditionals and lists

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, {useState} from "react";
+
+const DUMMY_EXPENSES = [
+    {id: '1', title: 'Car Insurance', amount: 294.67, date: new Date(2021,3,10)},
+    {id: '2', title: 'Home Insurance', amount: 2940.67, date: new Date(2021,2,10)},
+    {id: '3', title: 'Food', amount: 400.67, date: new Date(2021,3,10)}
+]
 
 const App = () =>  {
-  const expenses = [
-      {title: 'Car Insurance', amount: 294.67, date: new Date(2021,3,10)},
-      {title: 'Home Insurance', amount: 2940.67, date: new Date(2021,2,10)},
-      {title: 'Food', amount: 400.67, date: new Date(2021,3,10)}
-  ]
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const onAddExpenseHandler = (expenseData) => {
-      console.log(expenseData);
+      setExpenses(prevExpenses => {
+          return [expenseData, ...prevExpenses];
+      });
     }
 
   return (
